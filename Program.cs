92007ab689c7f5ac4531018ed098ad6272f3c5d7
Zachary Gattrell <zacharygattrell@gmail.com>
2020-09-04 13:40:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter height in inches.");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of Steps.");
+            int steps = int.Parse(Console.ReadLine());
+            double strideLengthFt = (height * 0.413)/12;
+            double milesWalked = (strideLengthFt * steps)/5280;
+            Console.WriteLine("Total Miles Walked is " + milesWalked + " miles.");
+
+
+        }
+    }
+}
